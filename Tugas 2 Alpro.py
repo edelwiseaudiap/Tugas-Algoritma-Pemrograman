9ffb2fe18e8292a871156fa190a74e7a7d8c91de
edelwiseaudiap <151481668+edelwiseaudiap@users.noreply.github.com>
2023-11-29 10:02:02 +0800

    Add files via upload

---
+++ Tugas 2 Alpro.py
@@ -0,0 +1,10 @@
+#input data nilai acak
+data_nilai = [3, 10, 6, 2, 8, 1, 5, 11]
+
+#mengurutkan data secara ascending
+data_nilai.sort()
+print("Mengurutkan data secara ascending: ", data_nilai)
+
+#mengurutkan data secara descending
+data_nilai.sort(reverse=True)
+print("Mengurutkan data secara descending: ", data_nilai)
